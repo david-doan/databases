20e5e281c19df031c913b8001a05cda0bc766431
david-doan <a.david.doan@gmail.com>
2016-05-13 20:06:12 -0700

    able to query database and send msg to client

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,18 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+dbConnection = mysql.createConnection({
+  user: 'root',
+  password: '1234',
+  database: 'chat'  
+});
 
+dbConnection.connect(err => {
+  if (err) {
+    console.log('Error connecting to Db', err);
+    return;
+  }
+  console.log('Connection established');
+});
+
+exports.DB = dbConnection;
--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,18 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function (req, res) {
+      //connect to db
+      db.DB.query('select * from messages', (err, row) => {
+        if (err) {
+          console.log('ERROR!!! Cannot query DB for messages');
+        }
+        console.log('Successful send from DB');
+        res.status(200).send({results:row});
+      });
+      //get the stuff
+      //respond
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
