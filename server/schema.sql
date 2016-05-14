7a7f7d44bc4a4262ba8954f45f1f62bb7cbba72c
david-doan <a.david.doan@gmail.com>
2016-05-13 17:31:54 -0700

    creates mysql tables, progress on server-spec-tests

--- server/schema.sql
+++ server/schema.sql
@@ -1,11 +1,41 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+CREATE TABLE users (
+  id int not null auto_increment,
+  name varchar(20),
+  primary key(id)
 );
 
+CREATE TABLE room (
+  id int not null auto_increment,
+  name varchar(20),
+  primary key(id)
+);
+
+CREATE TABLE friend (
+  id int not null auto_increment,
+  user int,
+  friend int,
+  primary key (id),
+  foreign key (user) references users(id),
+  foreign key (friend) references users(id)
+);
+
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id int not null auto_increment,
+  id_user int,
+  id_room int,
+  msg varchar(140),
+  createAt timestamp,
+  primary key (id),
+  FOREIGN KEY (id_user) REFERENCES users(id),
+  FOREIGN KEY (id_room) REFERENCES room(id)
+);
 /* Create other tables and define schemas for them here! */
 
 
